af08c2eec92ca1c0f59313edbd11ae09e38de53f
Krasnoffsky <47518567+Krasnoffsky@users.noreply.github.com>
2020-11-27 22:06:49 +0700

    Started Lab9 OS and desribed functions for Peterson

---
+++ Operation Systems/Lab9/lab9_os/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+
+bool readyFlags[2] = {false, false};
+
+int turn = 0;
+
+void InterCriticalRegion(int threadId)
+{
+
+}
+
+void ExitCriticalRegion(int threadId)
+{
+
+}
+
+void func1(void)
+{
+
+}
+
+void func2(void)
+{
+
+}
+
+int main(void)
+{
+
+    return 0;
+}
+
