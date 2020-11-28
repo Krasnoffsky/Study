781ffb8bf16198a18d9d704bd06894bf14cecf9d
Krasnoffsky <47518567+Krasnoffsky@users.noreply.github.com>
2020-11-28 19:20:44 +0700

    Completed Lab10 for OS

--- Operation Systems/Lab10/lab10_os/deadlock.cpp
+++ Operation Systems/Lab10/lab10_os/deadlock.cpp
@@ -3,34 +3,34 @@
 #include <stdio.h>
 
 HANDLE hMutex1, hMutex2;
-double sh1=0.0;
-int sh2=0;
+double sh1 = 0.0;
+int sh2 = 0;
 void Thread(void* pParams);
 
    int main(void) {
-    hMutex1=CreateMutex(NULL,FALSE,NULL);
-    hMutex2=CreateMutex(NULL,FALSE,NULL);
+    hMutex1 = CreateMutex(NULL,FALSE,NULL);
+    hMutex2 = CreateMutex(NULL,FALSE,NULL);
 
     _beginthread( Thread, 0, NULL );
 
-    while( 1){
-        WaitForSingleObject(hMutex1, INFINITE);//захват
-        printf("%g\n",sh1);
-        WaitForSingleObject(hMutex2, INFINITE);//захват
-        printf("%d\n",sh2);
-        ReleaseMutex(hMutex2);//освобождение
-        ReleaseMutex(hMutex1);//освобождение
+    while(1){
+        WaitForSingleObject(hMutex1, INFINITE);
+        printf("%g\n", sh1);
+        ReleaseMutex(hMutex1);
+        WaitForSingleObject(hMutex2, INFINITE);
+        printf("%d\n", sh2);
+        ReleaseMutex(hMutex2);
     }
     return 0;
 }
 
-void Thread( void* pParams ){
+void Thread(void* pParams){
     while (1){
-        WaitForSingleObject(hMutex2, INFINITE);//захват мьютекса
+        WaitForSingleObject(hMutex1, INFINITE);
+        sh1 += 0.1;
+        ReleaseMutex(hMutex1);
+        WaitForSingleObject(hMutex2, INFINITE);
         sh2++;
-        WaitForSingleObject(hMutex1, INFINITE);//захват мьютекса
-        sh1+=0.1;
-        ReleaseMutex(hMutex1); //освобождение мьютекса
-        ReleaseMutex(hMutex2); //освобождение мьютекса
+        ReleaseMutex(hMutex2);
     }
  }
