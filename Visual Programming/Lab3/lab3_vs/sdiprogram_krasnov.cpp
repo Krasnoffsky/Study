ef998cc4d9b19b009b3e638fdebead22be3d9a85
Krasnoffsky <47518567+Krasnoffsky@users.noreply.github.com>
2020-11-30 22:04:55 +0700

    Update lab3

--- Visual Programming/Lab3/lab3_vs/sdiprogram_krasnov.cpp
+++ Visual Programming/Lab3/lab3_vs/sdiprogram_krasnov.cpp
@@ -1,14 +1,2 @@
 #include "sdiprogram_krasnov.h"
 #include "ui_sdiprogram_krasnov.h"
-
-SDIProgram_Krasnov::SDIProgram_Krasnov(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::SDIProgram_Krasnov)
-{
-    ui->setupUi(this);
-}
-
-SDIProgram_Krasnov::~SDIProgram_Krasnov()
-{
-    delete ui;
-}
