719ccb4bee43f35551f082753ec9a5eb4efc08a8
Krasnoffsky <47518567+Krasnoffsky@users.noreply.github.com>
2020-12-06 18:47:34 +0700

    Add OMP for Lab5 AVS

--- AVS/Lab5/lab5_avs/main.cpp
+++ AVS/Lab5/lab5_avs/main.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <iostream>
 #include <pthread.h>
+#include <omp.h>
 
 using namespace std;
 
@@ -57,7 +58,7 @@ double MultipicMatrix(int size, int x_pos, int y_pos)
     return res;
 }
 
-void* DGEMM_BLAS(void* args)
+void* DGEMM_BLAS_pt(void* args)
 {
     int start = atoi(((char**)args)[0]);
     int end = atoi(((char**)args)[1]);
@@ -69,6 +70,14 @@ void* DGEMM_BLAS(void* args)
     }
 
     pthread_exit(0);
+}
+
+void DGEMM_BLAS_omp(int start, int end, int size) {
+    for(int i = start; i < end; i++) {
+        for(int j = 0; j < size; j++){
+            resMatrix[i][j] = MultipicMatrix(size, i, j);
+        }
+    }
 }
 
 int main(int argc, char** argv)
@@ -113,22 +122,36 @@ int main(int argc, char** argv)
     for (int i = 0; i < num_of_threads - 1; i++){
         snprintf(args[0], 10, "%d", start_i);
         snprintf(args[1], 10, "%d", stop_i);
-        pthread_create(&threads[i], NULL, DGEMM_BLAS, args);
+        pthread_create(&threads[i], NULL, DGEMM_BLAS_pt, args);
         start_i = stop_i;
         stop_i += step;
     }
 
     snprintf(args[0], 10, "%d", start_i);
     snprintf(args[1], 10, "%d", matrix_size);
-    pthread_create(&threads[num_of_threads - 1], NULL, DGEMM_BLAS, args);
+    pthread_create(&threads[num_of_threads - 1], NULL, DGEMM_BLAS_pt, args);
 
     for (int i = 0; i < num_of_threads; i++){
         pthread_join(threads[i], NULL);
     }
 
     BenchTimer("STOP");
+    cout << "POSIX Threads Time: " << timeCounter;
 
-    cout << "Time: " << timeCounter;
+    BenchTimer("Start");
+
+    #pragma omp parallel
+        {
+            #pragma omp for
+            for(int i = 0; i < num_of_threads; i++) {
+                if (i == num_of_threads - 1) DGEMM_BLAS_omp(step * (num_of_threads - 1), matrix_size, matrix_size);
+                else DGEMM_BLAS_omp(i * step, (i + 1) * step, matrix_size);
+            }
+        }
+
+    BenchTimer("STOP");
+
+    cout << "OpenMP Time: " << timeCounter;
 
     delete aMatrix;
     delete bMatrix;
