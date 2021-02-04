ee5e4fe1fda6f292834a6733e84f6c41d9d0fa15
Ilia Krasnov <senior00krasnoff@gmail.com>
2021-02-04 20:43:35 +0700

    Start lab1 Optim with class for work with fractions

---
+++ Course 3 Sem 2/Optimization/Lab1/simplefrac.h
@@ -0,0 +1,14 @@
+class SimpleFrac
+{
+    public:
+        SimpleFrac();
+        ~SimpleFrac();
+
+        int a, b;
+
+        void FracPlus(SimpleFrac t1, SimpleFrac t2);
+        void FracMinus(SimpleFrac t1, SimpleFrac t2);
+        void FracMulti(SimpleFrac t1, SimpleFrac t2);
+        void FracDiv(SimpleFrac t1, SimpleFrac t2);
+
+};
